7b1da60e4bd697645365a886f7058c6471d79bda
AbdelqaderMuhammad <abdelqadermuh@gmail.com>
2021-10-16 13:14:02 +0200

    cucumber full features with testNG

---
+++ src/test/java/steps/HelloWorld.java
@@ -0,0 +1,55 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class HelloWorld {
+
+    @Before
+    public void runs_before_each_scenario(){
+        System.out.println("Using before as hook in cucumber");
+    }
+
+    @Given("hello world")
+    public void hello_world() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("# Hello world using cucumber");
+    }
+
+    @Given("I want to say blah.")
+    public void i_want_to_say_blah() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("## I want to say blah");
+    }
+
+    @Given("say {string}")
+    public void say(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("### Say " + string);
+    }
+
+    @Then("say multiple {string} using examples")
+    public void say_multiple_using_examples(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("#### Say multiple name using examples as " + string );
+    }
+
+    @After
+    public void do_something_after_scenario(){
+        System.out.println("using after hook each scenario");
+    }
+
+    @After("@hello_world")
+    public void only_execute_at_tag(){
+        System.out.println("Only execute after @hello_world tag");
+    }
+
+    @AfterStep
+    public void execute_after_each_step(){
+        System.out.println("----this will execute after each step---");
+    }
+
+}
---
+++ src/test/resources/features/helloCucumber.feature
@@ -0,0 +1,29 @@
+# tage can work before features or scenario or scenario outline or examples only.
+
+Feature: hello world with cucumber in java
+
+  @hello_world @Any_word
+  Scenario: just say hello world.
+    Given hello world
+
+  @Any_word
+  Scenario: just say any world
+    Given I want to say blah.
+
+  @certain_word @Any_word
+  Scenario: say a word.
+    Given say "abdelqader"
+
+  @With_examples @Any_word
+  Scenario Outline: say multiple words
+    Given say multiple "<names>" using examples
+    @other_names
+    Examples:
+    | names      |
+    | Ahmed      |
+    | muhammad   |
+    @only_me
+    Examples:
+    | names      |
+    | Abdelqader |
+    | muhammad   |
